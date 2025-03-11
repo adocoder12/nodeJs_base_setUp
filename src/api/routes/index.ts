a9ab7120986e17689025ea93fa90ecf28e1d32ba
adocoder12 <adonay.jadal@gmail.com>
2025-03-11 02:05:05 +0100

    routes , and app file fixed , aswell i had disable  x-powered-by  and setup to recieve json file type

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
 
+//imported Routes
+import userRoute from "./authRoute";
+
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.json({ message: "API routes: /auth  , /users" });
+  res.json({ message: "API routes: /auth " });
 });
+
+/*--- ROUTES IN USE ---*/
+
+router.use("/auth", userRoute);
 
 export default router;
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,32 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
+//routes
 import routes from "./api/routes/index";
+//middlewares
 import { notFound, errorHandler } from "./utils/middlewares";
 
 const app = express();
+
+app.disable("x-powered-by");
+app.use(express.json());
+app.use(cors());
+
+/*--- CORS ---*/
+
+// app.use("*", cors());
+app.use(function (req: Request, res: Response, next: NextFunction) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    `http://localhost:${process.env.REACT_URI}`
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Expose-Headers", "Authorization");
+
+  next();
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "API location: api/v1" });
